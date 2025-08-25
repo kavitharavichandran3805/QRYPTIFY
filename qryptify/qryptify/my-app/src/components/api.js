f2ffef84fb58d3dd7bb8ea7d28c3b4578a1e8ff8
Kavitha Ravichandran <149370106+kavitharavichandran3805@users.noreply.github.com>
2025-08-25 21:43:23 +0530

    Add files via upload

---
+++ qryptify/qryptify/my-app/src/components/api.js
@@ -0,0 +1,30 @@
+export async function api(endpoint,method,body=null){
+  try{
+      console.log("inside the api function")
+      const url = 'http://localhost:8000/api/' + endpoint + "/";
+      const options={
+        method:method,
+        headers:{
+          'Content-Type':'application/json'
+        },
+        credentials:'include'
+      }
+      if(body!==null){
+        options.body=JSON.stringify(body)
+      }
+      const response=await fetch(url,options)
+      if(response.ok){
+        const result=await response.json()
+        return result;
+      }
+      else{
+        return { error: `Error: ${response.statusText}` };
+      }
+  }
+  catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred. Please try again.');
+        return { error: error.message };
+    }
+
+}
